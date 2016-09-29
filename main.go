a25ffabe69357ae0701b6828df9fa8ec26db2143
Cosmin Rentea <c.rentea@tarent.de>
2016-09-29 14:06:45 +0300

    using p12 file

--- main.go
+++ main.go
@@ -7,24 +7,21 @@ import (
 )
 
 func main() {
-
-  cert, pemErr := certificate.FromPemFile("../cert.pem", "")
+  cert, pemErr := certificate.FromP12File("development-certificate.p12", "")
   if pemErr != nil {
-    log.Println("Cert Error:", pemErr)
+    log.Println("Certificate Error:", pemErr)
   }
 
   notification := &apns.Notification{}
   notification.DeviceToken = "11aa01229f15f0f0c52029d8cf8cd0aeaf2365fe4cebc4af26cd6d76b7919ef7"
-  notification.Topic = "com.sideshow.Apns2"
-  notification.Payload = []byte(`{"aps":{"alert":"Hello!"}}`) // See Payload section below
+  notification.Topic = "APNS Test"
+  notification.Payload = []byte(`{"aps":{"alert":"Hello!"}}`)
 
-  client := apns.NewClient(cert).Production()
+  client := apns.NewClient(cert).Development()
   res, err := client.Push(notification)
-
   if err != nil {
     log.Println("Error:", err)
     return
   }
-
   log.Println("APNs ID:", res.ApnsID)
 }
