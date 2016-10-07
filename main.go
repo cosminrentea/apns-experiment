dadc6e550549dd2f8233cf6ad0966462d72f773a
Cosmin Rentea <c.rentea@tarent.de>
2016-10-07 11:54:03 +0300

    refactoring error-handling; extracted func to get an apns-client

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	apns "github.com/sideshow/apns2"
@@ -13,6 +14,8 @@ import (
 
 const (
 	defaultCertFileName = "development-certificate.p12"
+	msgAPSNNotSent      = "APNS notification was not sent"
+	errAPSNNotSent      = errors.New(msgAPSNNotSent)
 )
 
 func init() {
@@ -24,7 +27,6 @@ func init() {
 type APNSConfig struct {
 	CertFileName string
 	CertPassword string
-	Topic        string
 }
 
 func main() {
@@ -34,39 +36,44 @@ func main() {
 	cfg := APNSConfig{
 		CertFileName: defaultCertFileName,
 		CertPassword: os.Getenv("APNS_CERT_PASSWORD"),
-		Topic:        os.Getenv("APNS_TOPIC"),
 	}
+	topic := os.Getenv("APNS_TOPIC")
 	deviceToken := os.Getenv("APNS_DEVICE_TOKEN")
-
+	cl := getAPNSClient(cfg, false)
 	p := payload.NewPayload().
 		AlertTitle("REWE Sonderrabatt").
 		AlertBody("Sie haben ein Sonderrabatt von 50% für das neue iPhone 8 bekommen!").
 		ContentAvailable()
-
-	sendAPNSNotification(cfg, deviceToken, p)
+	sendAPNSNotification(cl, topic, deviceToken, p)
 }
 
-func sendAPNSNotification(c APNSConfig, deviceToken string, p *payload.Payload) {
-	cert, errCert := certificate.FromP12File(c.CertFileName, c.CertPassword)
+func getAPNSClient(cfg *APNSConfig, production bool) *apns.Client {
+	cert, errCert := certificate.FromP12File(cfg.CertFileName, cfg.CertPassword)
 	if errCert != nil {
 		log.WithError(errCert).Error("APNS certificate error")
 	}
+	if production {
+		return apns.NewClient(cert).Production()
+	}
+	return apns.NewClient(cert).Development()
+}
 
-	notification := &apns.Notification{}
-	notification.Priority = apns.PriorityHigh
-	notification.Topic = c.Topic
-	notification.DeviceToken = deviceToken
-	notification.Payload = p
-
-	client := apns.NewClient(cert).Development()
-	response, errPush := client.Push(notification)
+func sendAPNSNotification(cl *apns.Client, topic string, deviceToken string, p *payload.Payload) error {
+	notification := &apns.Notification{
+		Priority:    apns.PriorityHigh,
+		Topic:       topic,
+		DeviceToken: deviceToken,
+		Payload:     p,
+	}
+	response, errPush := cl.Push(notification)
 	if errPush != nil {
-		log.WithError(errPush).Error("APNS error when pushing notification")
-		return
+		log.WithError(errPush).Error("APNS error when trying to push notification")
+		return errPush
 	}
-	if response.Sent() {
-		log.WithField("id", response.ApnsID).Debug("APNS notification successfully sent")
-	} else {
-		log.WithField("id", response.ApnsID).Error("APNS notification was not sent")
+	if !response.Sent() {
+		log.WithField("id", response.ApnsID).Error(msgAPSNNotSent)
+		return errAPSNNotSent
 	}
+	log.WithField("id", response.ApnsID).Debug("APNS notification successfully sent")
+	return nil
 }
